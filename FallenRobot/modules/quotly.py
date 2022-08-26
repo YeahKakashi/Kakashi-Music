a33f7c7532a4840beb05348437fb64e2ce6b7b39
Tɪɢᴇʀ Nᴇᴛᴡᴏʀᴋ™ <111215574+TigerNetwork@users.noreply.github.com>
2022-08-27 01:48:01 +0530

    Update quotly.py

--- FallenRobot/modules/quotly.py
+++ FallenRobot/modules/quotly.py
@@ -29,8 +29,8 @@ from pyrogram.types import Message
 
 
 
-__MODULE__ = "Quotly"
-__HELP__ = """
+__mod_name__ = "Quotly"
+__help__ = """
 /q - To quote a message.
 /q [INTEGER] - To quote more than 1 messages.
 /q r - to quote a message with it's reply
